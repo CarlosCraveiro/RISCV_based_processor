7e9a265ed4a2119176257a568fa98be4867f1396
CarlosCraveiro <carlos.craveiro@usp.br>
2022-10-26 15:44:45 -0300

    Specifies the size of empty buses at 8NxN mux

--- src/ALU.v
+++ src/ALU.v
@@ -7,23 +7,23 @@ module ALU #(WIDTH = 1)
 
   wire [(WIDTH - 1):0] SrcB_2comp; 
   wire [(WIDTH - 1):0] SrcB_var;
-  wire [(WIDTH-1):0] Sum;
-  wire [(WIDTH-1):0] zExtended;
-  wire [(WIDTH-1):0] OrOperation;
-  wire [(WIDTH-1):0] AndOperation;
+  wire [(WIDTH - 1):0] Sum;
+  wire [(WIDTH - 1):0] zExtended;
+  wire [(WIDTH - 1):0] OrOperation;
+  wire [(WIDTH - 1):0] AndOperation;
+  wire [(WIDTH - 1):0] DoNothing;
 
   two_complement #(.N(WIDTH)) twc (SrcB, SrcB_2comp);
   
   mux_2NxN #(.N(WIDTH)) m1({SrcB_2comp, SrcB}, ALUControl[0], SrcB_var);
 
   assign Sum = SrcA + SrcB_var;
-  N_zero_extend #(.N(WIDTH)) zExt1(Sum[WIDTH-1], zExtended);
+  N_zero_extend #(.N(WIDTH)) zExt1(Sum[WIDTH - 1], zExtended);
 
   assign OrOperation = SrcA | SrcB;
   assign AndOperation = SrcA & SrcB;
 
-  mux_8NxN #(.N(WIDTH)) m2({0,0,zExtended,0,OrOperation,AndOperation,Sum,Sum},ALUControl,ALUResult);
-  assign zero = ~ALUResult[WIDTH-1];
-  // More code goes here...
+  mux_8NxN #(.N(WIDTH)) m2({DoNothing, DoNothing, zExtended, DoNothing, OrOperation, AndOperation, Sum, Sum}, ALUControl, ALUResult);
+  assign zero = ~ALUResult[WIDTH - 1];
 
 endmodule
