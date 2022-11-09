f40998f5c9e39e75c7ce2d1f8e3adcdb8c8f81fb
CarlosCraveiro <carlos.craveiro@usp.br>
2022-11-09 15:43:59 -0300

    creates register_bank declaration

---
+++ src/register_bank.v
@@ -0,0 +1,14 @@
+module RegBank
+#(parameter WIDTH = 4, SIZE = 3)
+(
+ input            clk,
+ input            we3,
+ input [SIZE-1:0] A1,
+ input [SIZE-1:0] A2,
+ input [SIZE-1:0] A3,
+ input [WIDTH-1:0] WD3,
+ output [WIDTH-1:0] RD1,
+ output [WIDTH-1:0] RD2
+);
+
+endmodule
