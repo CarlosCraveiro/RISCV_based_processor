78dfc79a270248f190a5904ecf02a3f55cbda83e
CarlosCraveiro <carlos.craveiro@usp.br>
2022-11-09 15:26:15 -0300

    adds register implementation

--- src/register.v
+++ src/register.v
@@ -1,10 +1,16 @@
 module register
 #( parameter WIDTH = 1 )
    (
-    input  clk,
-    input  in_data,
-    input  enable,
-    output ou_data
+    input clk,
+    input [WIDTH-1:0] in_data,
+    input enable,
+    output reg [WIDTH-1:0] ou_data
     );
 
+   always @(posedge clk) begin
+      if(enable)
+        ou_data <= in_data;
+      else
+        ou_data <= ou_data;
+   end
 endmodule
